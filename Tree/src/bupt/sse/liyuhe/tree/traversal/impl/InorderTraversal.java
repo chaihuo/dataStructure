a59701396277f11e524e4b7be3aebf65277aff84
chaihuo <chaihuobang@gmail.com>
2013-09-08 15:12:41 +0800

    non-recursion way of in order traversal bi-tree(need to clear up function implement)

--- Tree/src/bupt/sse/liyuhe/tree/traversal/impl/InorderTraversal.java
+++ Tree/src/bupt/sse/liyuhe/tree/traversal/impl/InorderTraversal.java
@@ -1,5 +1,7 @@
 package bupt.sse.liyuhe.tree.traversal.impl;
 
+import java.util.Stack;
+
 import bupt.sse.liyuhe.tree.BinaryTreeNode;
 import bupt.sse.liyuhe.tree.traversal.Traversal;
 
@@ -23,8 +25,51 @@ public class InorderTraversal implements Traversal {
 
 	@Override
 	public void traversal(BinaryTreeNode root) {
-		// TODO Auto-generated method stub
-
+		/**
+		 * store root after traversal its left child
+		 */
+		Stack<BinaryTreeNode> toTraversal = new Stack<BinaryTreeNode>();
+		downToLeft(toTraversal, root);
+		while(!toTraversal.isEmpty()) {
+			ifNext(toTraversal);
+		}
+	}
+	
+	/**
+	 * traversal a bi-tree to its leftest child
+	 * @param toTraversal
+	 * @param currentNode
+	 */
+	private void downToLeft(Stack<BinaryTreeNode> toTraversal, BinaryTreeNode currentNode) {
+		
+		while (currentNode != null) {
+			if (currentNode.getLeft() != null) {
+				toTraversal.add(currentNode);
+				currentNode = currentNode.getLeft();
+			} else {
+				System.out.println(currentNode.getItem());
+				return;
+			}
+			
+		}
+		
+		
+	}
+	
+	/**
+	 * get a node from toTraversal
+	 * @param toTraversal
+	 */
+	private void ifNext(Stack<BinaryTreeNode> toTraversal) {
+		BinaryTreeNode currentNode = toTraversal.pop();
+		if (currentNode.getRight() != null) {
+			System.out.println(currentNode.getItem());
+			downToLeft(toTraversal, currentNode.getRight());
+			return;
+		} else {
+			System.out.println(currentNode.getItem());
+			return;
+		}
 	}
 
 }
