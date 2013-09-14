a70c6de1ef0aa7d5e6855ec2cf47d71ba4349024
chaihuo <chaihuobang@gmail.com>
2013-09-14 16:02:17 +0800

    Add none-recursive post order traversal function.

--- Tree/src/bupt/sse/liyuhe/tree/traversal/impl/PostorderTraversal.java
+++ Tree/src/bupt/sse/liyuhe/tree/traversal/impl/PostorderTraversal.java
@@ -1,5 +1,7 @@
 package bupt.sse.liyuhe.tree.traversal.impl;
 
+import java.util.Stack;
+
 import bupt.sse.liyuhe.tree.BinaryTreeNode;
 import bupt.sse.liyuhe.tree.traversal.Traversal;
 
@@ -23,8 +25,77 @@ public final class PostorderTraversal implements Traversal {
 
 	@Override
 	public void traversal(BinaryTreeNode root) {
-		// TODO Auto-generated method stub
-
+		if (root == null) {
+			return;
+		}
+		Stack<BinaryTreeNode> toTraversal = new Stack<BinaryTreeNode>();
+		
+		toTraversal.add(root);
+		
+		BinaryTreeNode lastNode = null;
+		while (!toTraversal.isEmpty()) {
+			BinaryTreeNode currentNode = toTraversal.pop();
+			
+			/**
+			 * 1. if leaf, sysout & continue
+			 */
+			if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+				System.out.println(currentNode.getItem());
+				lastNode = currentNode;
+				continue;
+			}
+			
+			/**
+			 * 2. if have 2 children
+			 */
+			if (currentNode.getLeft() != null && currentNode.getRight() != null) {
+				if (currentNode.getLeft().equals(lastNode) || currentNode.getRight().equals(lastNode)) {
+					System.out.println(currentNode.getItem());
+					lastNode = currentNode;
+					continue;
+				} else {
+					loadNodes(toTraversal, currentNode);
+					continue;
+				}
+			}
+			
+			/**
+			 * 3. if only left child
+			 */
+			if (currentNode.getLeft() != null && currentNode.getRight() == null) {
+				if (currentNode.getLeft().equals(lastNode)) {
+					System.out.println(currentNode.getItem());
+					lastNode = currentNode;
+					continue;
+				} else {
+					loadNodes(toTraversal, currentNode);
+					continue;
+				}
+			}
+			
+			/**
+			 * 4. if only right child
+			 */
+			if (currentNode.getLeft() == null && currentNode.getRight() != null) {
+				if (currentNode.getRight().equals(lastNode)) {
+					System.out.println(currentNode.getItem());
+					lastNode = currentNode;
+					continue;
+				} else {
+					loadNodes(toTraversal, currentNode);
+					continue;
+				}
+			}
+		}
 	}
 
+	private void loadNodes(Stack<BinaryTreeNode> toTraversal, BinaryTreeNode root) {
+		toTraversal.add(root);
+		if (root.getRight() != null) {
+			toTraversal.add(root.getRight());
+		}
+		if (root.getLeft() != null) {
+			toTraversal.add(root.getLeft());
+		}
+	}
 }
